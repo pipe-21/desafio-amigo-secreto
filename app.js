60ac1cb76caf5bda3fc3ea163b05018430873f41
Andres Felipe <andresfreyes1@gmail.com>
2025-09-02 13:12:01 -0500

    Actualiza app.js con el código completo

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = ["Ana", "Bruno", "Carla", "Daniel", "Eva", "Fernando"];
+
+function agregarAmigo() {
+  const input = document.getElementById("amigo");
+  const nombre = input.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  amigos.push(nombre);
+
+  input.value = "";
+
+  mostrarListaAmigos();
+}
+
+function mostrarListaAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos para sortear.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  const nombreSorteado = amigos[indiceAleatorio];
+
+  document.getElementById("resultado").innerHTML =
+    `<li>El amigo sorteado es: ${nombreSorteado}</li>`;
+}
+
+
+
+
